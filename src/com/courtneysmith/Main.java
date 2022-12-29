417dc01260798775fe71c7a3949a4cc92911cec1
smith494 <court14104@gmail.com>
2022-12-28 21:39:11 -0600

    End of the UDP discussion

--- src/com/courtneysmith/Main.java
+++ src/com/courtneysmith/Main.java
@@ -1,5 +1,37 @@
+package com.courtneysmith;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            InetAddress address = InetAddress.getLocalHost();
+            DatagramSocket datagramSocket = new DatagramSocket();
+
+            Scanner scanner = new Scanner(System.in);
+            String echoString;
+
+            do{
+                System.out.println("Enter string to be echoed: ");
+                echoString = scanner.nextLine();
+
+                byte [] buffer = echoString.getBytes();
+
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address,7543);
+                datagramSocket.send(packet);
+
+                byte [] buffer2 = new byte[50];
+                packet = new DatagramPacket(buffer2, buffer2.length);
+                datagramSocket.receive(packet);
+                System.out.println("Text received is: " + new String(buffer2, 0, packet.getLength()));
+            }while (!echoString.equals("exit"));
+
+        }catch (SocketTimeoutException e){
+            System.out.println("The socket timed out");
+        }catch (IOException e){
+            System.out.println("Client error: " + e.getMessage());
+        }
     }
 }
